d5c06d74f1b3ebc8dcfd37b3305e846da89bdd4b
Rosabel Tan <rosabeltan530@icloud.com>
2023-06-12 08:25:00 -0700

    Initial Commit

---
+++ Git and Github/Git_and_GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_and_GithubApp.swift
+//  Git and Github
+//
+//  Created by Rosabel Tan on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Git_and_GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
